a9c3d10e8dc2a21d052affd833fc3d38620caccf
John Gruber <john.t.gruber@gmail.com>
2026-03-23 11:33:20 -0500

    Fix CORS middleware to allow POST, DELETE, OPTIONS and Content-Type header

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ security = HTTPBasic()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_headers=["Authorization"],
-    allow_methods=["GET"],
+    allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
 )
 
 # ---------------------------------------------------------------------------
